906b6706991b0f01a3c029e2a89b3d02754f61a0
alesmall <alessandrapiccolo23@gmail.com>
2025-06-28 15:40:11 +0200

    inizializzato il progetto

---
+++ src/main/java/IlTempioDeiPianeti.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * the Main class. it's the entry point of the application.
+ * 
+ * @author alessandra
+ * 
+ */
+public class IlTempioDeiPianeti {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
