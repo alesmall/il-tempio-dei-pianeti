70fd106e0739d6b7e13a6486f21184c1f6e3fdde
alesmall <alessandrapiccolo23@gmail.com>
2025-07-13 15:53:49 +0200

    aggiunta javadoc

---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"backend"},{"l":"engine"},{"l":"entity"},{"l":"gameplay"},{"l":"GUI"},{"l":"util"}];updateSearchResults();
